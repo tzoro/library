2284e44b2f67082b01368ee36da4856f4ae1dba2
Tino <tino@klassapp.com>
2020-11-26 19:30:46 +0100

    Books scaffolded

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_181917) do
+ActiveRecord::Schema.define(version: 2020_11_26_182937) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_authors_on_name", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "quantity"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["title"], name: "index_books_on_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_11_26_181917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "authors"
 end
